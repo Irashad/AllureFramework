42c09f36eb0efa8ff87bab3b98760d64759a3bdf
Irashad <reshad.nesirov5@gmail.com>
2022-04-26 20:08:56 +0400

    first commit

---
+++ src/test/java/LMP.java
@@ -0,0 +1,24 @@
+import io.qameta.allure.*;
+import org.testng.annotations.Test;
+
+@Epic("Calculator calculations")
+public class LMP {
+
+
+    @Description("This test case is built for testing calculation feature")
+    @Feature("Calculator")
+    @Story("Check sum is working properly")
+    @Test
+    public void sum()
+    {
+
+    }
+    @Description("This test case is built for testing calculation feature")
+    @Feature("Calculator")
+    @Story("check multiplation is working")
+    @Test
+    public void multiple()
+    {
+
+    }
+}
